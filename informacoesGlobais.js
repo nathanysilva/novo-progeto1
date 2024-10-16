818b70f941c5d0f9e4002a50fc331cf74f0147b4
nathanysilva <110236003+nathanysilva@users.noreply.github.com>
2024-10-16 09:12:40 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,18 @@
+const url='https://rawgithubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+
+//url='20.201.28.151/main/dados/grafico-principal.json';
+//modelo de endereço servidor de dados para o javaScript.
+
+async function visualizarIformacoesGlobais(){
+    const res = await fetch (url)
+    const dados = await res.json
+    const pessoasConectadas = (dados.total_pessoas_conectadas /1e9)
+    const pessoasNoMundo = (dados.tortal_pessoas_do_mundo/1e9)
+    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_de_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão em alguma rede social e passam em média <span>${dados.tempo_medio}</span>horas conectadas.`
+
+}
+
+visualizarIformacoesGlobais();

37f028c3393b5ca2caab44b6bb28e73dc3f3ab39
nathanysilva <110236003+nathanysilva@users.noreply.github.com>
2024-10-21 11:02:52 -0300

    Add files via upload

--- quantidadeUsuarioPorRede.js
+++ quantidadeUsuarioPorRede.js
@@ -20,3 +20,18 @@ async function quantidadeUsuariosPorRede() {
     }
     
     quantidadeUsuariosPorRede();
+
+    const laytout = {
+        Ploty_bgcolor: getcss('--bg-color');
+        paper_bgcolor: getcss ('--bg-coçor');
+        title: {
+            Text: 'Rede sociais com mais usuários',
+            x: 0,
+            font:{
+
+                color: getCSS('--primary-color'),
+                size: 30,
+            }
+
+        }
+    }
